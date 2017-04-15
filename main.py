e93d547201f12a7fa9e2457496d3a719b476ff41
rcochrell2 <rcochrell2@users.noreply.github.com>
2017-04-15 13:51:25 -0500

    Modified the fortunecookies like on video

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import webapp2
 import random
 
+def getRandomFortune():
+    fortunes = [
+        "I see alot of code and binary in your future",
+        "Eat more fortune cookies then anyother",
+        "You will go hunting for snake And get the Python"]
+
+    index = random.randint(0, 2)
+
+    return fortunes[index]
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         header = "<h1>Fortune Cookie</h1>"
 
-        lucky_number = random.randint(1, 100)
-        number_sentence = 'Your lucky number:' + str(lucky_number)
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = "<strong>" + str(random.randint(1, 100)) + "</strong>"
+        number_sentence = "Your lucky number: " + lucky_number
         number_paragraph = "<p>" + number_sentence + "</p>"
-        self.response.write(header + number_paragraph)
+
+        cookie_again_button = "<a href='.'><button>Another Cookie Please</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
